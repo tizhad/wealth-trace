e2f958df278542969b97c333d76f587594e38265
tina <tinarezanezhaad@gmail.com>
2026-04-30 14:10:43 +0200

    Initialize Angular application with app configuration and main entry point

---
+++ apps/tracker/src/app/app.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  imports: [RouterModule],
+  selector: 'app-root',
+  templateUrl: './app.html',
+  styleUrl: './app.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class App {}
